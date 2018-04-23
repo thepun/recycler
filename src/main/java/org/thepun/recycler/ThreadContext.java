33970d24448df1a6c77b287990f819a25a2dd056
ThePun <thepun599+myatlassian@gmail.com>
2018-04-23 19:49:15 +0300

    work in progress

--- src/main/java/org/thepun/recycler/ThreadContext.java
+++ src/main/java/org/thepun/recycler/ThreadContext.java
@@ -3,16 +3,20 @@ package org.thepun.recycler;
 final class ThreadContext {
 
     private static final int MAX_LOCAL_FREE = 1024;
+    private static final int MAX_LOCAL_FREE_MASK = MAX_LOCAL_FREE - 1;
 
 
     private final int registeredType;
+    private final TypeContext typeContext;
     private final RecyclableObject[] freeLocal;
     private final RecyclableObject[] freeOther;
-    private final TypeContext typeContext;
     private final RecyclableObjectFactory<?> factory;
 
     private int freeLocalCount;
-    private int freeOtherCount;
+    private long freeOtherReader;
+
+    // TODO: offset value
+    private long freeOtherWriters;
 
     ThreadContext(TypeContext typeContext) {
         this.typeContext = typeContext;
@@ -28,12 +32,22 @@ final class ThreadContext {
         return factory.createNew(registeredType);
     }
 
+    // TODO: add barrier
     RecyclableObject get() {
         if (freeLocalCount > 0) {
             int freeIndex = --freeLocalCount;
             RecyclableObject object = freeLocal[freeIndex];
             freeLocal[freeIndex] = null;
             return object;
+        }
+
+        long localFreeOtherReader = freeOtherReader;
+        long localFreeOtherWriters = freeOtherWriters;
+        if (localFreeOtherWriters > localFreeOtherReader) {
+            int index = (int) (localFreeOtherReader & MAX_LOCAL_FREE_MASK);
+            freeOther[index] = null;
+            localFreeOtherReader++;
+            freeOtherReader = localFreeOtherReader;
         }
 
         RecyclableObject object = typeContext.tryGetFreeGlobalObject();
@@ -45,14 +59,18 @@ final class ThreadContext {
         return object;
     }
 
+    // TODO: add barrier
     void addFreeObjectBackToOrigin(RecyclableObject object) {
 
-        //free.add(object);
+
     }
 
+    // TODO: add barrier
     void addFreeObjectForLocalUse(RecyclableObject object) {
-        //free.add(object);
+        if (freeLocalCount < MAX_LOCAL_FREE) {
+            freeLocal[freeLocalCount++] = object;
+        } else {
+            typeContext.addFreeObjectForGlobalUse(object);
+        }
     }
-
-
 }
